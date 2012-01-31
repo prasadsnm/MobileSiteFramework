10caa4a5da16e923cda3a0b8748a3840b66d781b
Craig Pfau <craigpfau@gmail.com>
2012-01-30 21:49:49 -0700

    Initial commit

---
+++ javascript/twitter.js
@@ -0,0 +1,21 @@
+$(function(){
+    $.ajax({
+        url: "https://api.twitter.com/1/statuses/user_timeline.json?user_id=189311078",
+        dataType: 'jsonp',
+        success: function(json_results){
+            console.log(json_results);
+            // Need to add UL on AJAX call or formatting of userlist is not displayed
+            $('#twitList').append('<ul data-role="listview"></ul>');
+            listItems = $('#twitList').find('ul');
+            $.each(json_results, function(key) {
+                html = '<img src="'+json_results[key].user.profile_image_url+'"/>';
+                html += '<div class="x-tweetanchor"></div><div class="tweet-bubble"><div class="tweet-content"><h2>'+json_results[key].user.name+'</h2><p><strong>'+json_results[key].text+'</strong></p>';
+                html += '<p>'+json_results[key].created_at+'</p>';
+                listItems.append('<li>'+html+'</li>');
+            });
+            // Need to refresh list after AJAX call
+            $('#twitList ul').listview();
+        }
+    });
+})
+
